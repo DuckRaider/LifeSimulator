90b88c0998e12beb2c4f8540a46d8db897a814f3
Alessio Ferretti <markusherzig208@gmail.com>
2024-03-05 12:07:08 +0100

    wdadawd

--- LifeSim/src/App.jsx
+++ LifeSim/src/App.jsx
@@ -1,22 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import { Home } from "./components/Home/Home"
 import { Login } from "./components/Auth/Login"
 import { Register } from "./components/Auth/Register";
 import { UserProvider } from "./context/UserContext";
+import Routes from "./Routes/Routes";
 
 function App() {
   return (
     <UserProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" index element={<Home/>}/>
-          <Route path="/home" index element={<Home/>}/>
-          {/* <Route path='/*' element={<NotFound/>}/> */}
-          {/* <Route path='/trails' element={<TrailPage/>}/> */}
-          <Route path='/login' element={<Login/>}/>
-          <Route path='/register' element={<Register/>}/>
-        </Routes>
-      </BrowserRouter>
+      <Routes/>
     </UserProvider>
   )
 }
